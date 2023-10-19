f689b63ce4dc3ff0ce2f8d32fc40e599344122cf
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2023-10-19 11:11:01 +0530

    Improved UI

--- TicTacToe.java
+++ TicTacToe.java
@@ -4,7 +4,7 @@ import java.util.*;
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
 
-public class TicTacToe implements ActionListener{
+public class TicTacToe implements ActionListener, MouseListener{
     JFrame jFrame = new JFrame("Tic Tac Toe");
     JPanel titlePanel = new JPanel();
     JPanel buttonPanel = new JPanel();
@@ -12,15 +12,21 @@ public class TicTacToe implements ActionListener{
     JButton[] buttons = new JButton[9];
     boolean player1Turn=true, hasWon=false, hasChance=false;
 
+    //Theme colors
+    Color color1 = new Color(236, 154, 83);
+    Color color2 = new Color(255, 255, 255);
+    Color color3 = new Color(252, 65, 65);
+    Color color4 = new Color(231, 112, 22);
+
     public TicTacToe(){
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setSize(400, 400);
-        jFrame.getContentPane().setBackground(new Color(0, 0, 0));
+        jFrame.getContentPane().setBackground(color2);
         jFrame.setLayout(new BorderLayout());
 
-        textField.setBackground(new Color(0, 10, 0));
-        textField.setForeground(new Color(255, 119, 0));
-        textField.setFont(new Font("Ink Free", Font.BOLD, 30));
+        textField.setBackground(color2);
+        textField.setForeground(color1);
+        textField.setFont(new Font("Ink Free", Font.BOLD, 40));
         textField.setHorizontalAlignment(JLabel.CENTER);
         textField.setVerticalAlignment(JLabel.CENTER);
         textField.setText("Tic-Tac-Toe");
@@ -32,17 +38,18 @@ public class TicTacToe implements ActionListener{
             buttons[i].setFont(new Font("MV Boli", Font.BOLD, 25));
             buttons[i].setFocusable(false);
             buttons[i].addActionListener(this);
-            buttons[i].setBackground(new Color(255, 119, 0));
-            buttons[i].setBorder(new BevelBorder(BevelBorder.LOWERED, Color.black, Color.black, Color.black, Color.black));
+            buttons[i].addMouseListener(this);
+            buttons[i].setBackground(color1);
+            buttons[i].setBorder(new BevelBorder(BevelBorder.LOWERED, color1, color1, color1, color1));
         }
 
         titlePanel.setLayout(new FlowLayout());
-        titlePanel.setBackground(Color.black);
-        titlePanel.setBounds(0, 30, 400, 400);
+        titlePanel.setBackground(color2);
+        titlePanel.setBounds(0, 30, 300, 300);
         titlePanel.add(textField);
 
-        buttonPanel.setLayout(new GridLayout(3, 3));
-        buttonPanel.setBackground(new Color(255, 119, 0));
+        buttonPanel.setLayout(new GridLayout(3, 3, 6, 6));
+        buttonPanel.setBackground(color2);
 
         jFrame.add(buttonPanel);
         jFrame.add(titlePanel, BorderLayout.NORTH);
@@ -57,24 +64,22 @@ public class TicTacToe implements ActionListener{
     public void actionPerformed(ActionEvent e){
         for(int i=0; i<9; i++){
             if(e.getSource() == buttons[i]){
+                buttons[i].setForeground(color2);
                 if(player1Turn){
-                    if(Objects.equals(buttons[i].getText(), "")){
-                        buttons[i].setForeground(new Color(0, 0, 0));
+                    if(buttons[i].getText().isEmpty()){
                         buttons[i].setText("X");
                         player1Turn = false;
                         textField.setText("O turn");
-                        check();
                     }
                 }
                 else{
-                    if(Objects.equals(buttons[i].getText(), "")){
-                        buttons[i].setForeground(new Color(0, 0, 0));
+                    if(buttons[i].getText().isEmpty()){
                         buttons[i].setText("O");
                         player1Turn = true;
                         textField.setText("X turn");
-                        check();
                     }
                 }
+                check();
             }
         }
     }
@@ -131,13 +136,69 @@ public class TicTacToe implements ActionListener{
     }
 
     public void win(int a, int b, int c, String winner){
-        buttons[a].setBackground(Color.WHITE);
-        buttons[b].setBackground(Color.WHITE);
-        buttons[c].setBackground(Color.WHITE);
+        buttons[a].setBackground(color3);
+        buttons[a].setBorder(new BevelBorder(BevelBorder.LOWERED, color3, color3, color3, color3));
+        buttons[b].setBackground(color3);
+        buttons[b].setBorder(new BevelBorder(BevelBorder.LOWERED, color3, color3, color3, color3));
+        buttons[c].setBackground(color3);
+        buttons[c].setBorder(new BevelBorder(BevelBorder.LOWERED, color3, color3, color3, color3));
         for(int i=0; i<9; i++){
             buttons[i].setEnabled(false); //cannot play the game again
         }
         textField.setText(winner + "Wins");
         hasWon = true;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        for(int i=0; i<9; i++){
+            if(e.getSource() == buttons[i]){
+                buttons[i].setBackground(color3);
+                buttons[i].setForeground(color2);
+                buttons[i].setBorder(new BevelBorder(BevelBorder.LOWERED, color3, color3, color3, color3));
+            }
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        for(int i=0; i<9; i++){
+            if(e.getSource() == buttons[i]){
+                buttons[i].setBackground(color2);
+                buttons[i].setForeground(color3);
+                buttons[i].setBorder(new BevelBorder(BevelBorder.LOWERED, color2, color2, color2, color2));
+            }
+        }
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        for(int i=0; i<9; i++){
+            if(e.getSource() == buttons[i]){
+                buttons[i].setBackground(color1);
+                buttons[i].setForeground(color2);
+                buttons[i].setBorder(new BevelBorder(BevelBorder.LOWERED, color1, color1, color1, color1));
+            }
+        }
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        for(int i=0; i<9; i++){
+            if(e.getSource() == buttons[i]){
+                buttons[i].setBackground(color3);
+                buttons[i].setBorder(new BevelBorder(BevelBorder.LOWERED, color3, color3, color3, color3));
+            }
+        }
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        for(int i=0; i<9; i++){
+            if(e.getSource() == buttons[i]){
+                buttons[i].setBackground(color1);
+                buttons[i].setBorder(new BevelBorder(BevelBorder.LOWERED, color1, color1, color1, color1));
+            }
+        }
     }
 }
